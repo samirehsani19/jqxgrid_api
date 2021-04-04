85112afa0bccca0d899daf9535bfc5e240756b5a
samirehsani19 <samir.ehsani@learnet.se>
2021-04-04 17:00:05 +0200

    basic grid

---
+++ jqxgrid_api/jqxgrid_api/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using jqxgrid_api.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jqxgrid_api.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        DataContext ctx = new DataContext();
+
+       // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            var data = ctx.Employees.ToList<Employee>();
+            return Json(new { rows= data}, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Add(Employee emp)
+        {
+            try
+            {
+                ModelState.Remove(nameof(emp.Id)); // remove id 
+                ctx.Employees.Add(emp);
+                ctx.SaveChanges();
+                return RedirectToAction("GetData");
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+        
+        public ActionResult Edit(int id, Employee emp)
+        {
+            try
+            {
+                ctx.Entry(emp).State = EntityState.Modified;
+                ctx.SaveChanges();
+                return RedirectToAction("GetData");
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        } 
+        
+        public ActionResult Delete(Employee emp)
+        {
+            try
+            {
+                ctx.Employees.Remove(emp);
+                ctx.SaveChanges();
+                return RedirectToAction("GetData");
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ jqxgrid_api/jqxgrid_api/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using jqxgrid_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace jqxgrid_api
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var emp = modelBuilder.Entity<Employee>();
+            emp.HasKey(x => x.Id);
+        }
+    }
+}
