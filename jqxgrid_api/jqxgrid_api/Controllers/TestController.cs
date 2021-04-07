94a96d45f1c11decbf944918b96f69dd3179a8fd
Samir Ehsani <samirehsani19@gmail.com>
2021-04-07 22:26:14 +0200

    added file

--- jqxgrid_api/jqxgrid_api/Controllers/TestController.cs
+++ jqxgrid_api/jqxgrid_api/Controllers/TestController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 
@@ -22,7 +24,20 @@ namespace jqxgrid_api.Controllers
         //    var data = ctx.Employees.ToList();
         //    return PartialView("Test/_AjaxTest", data);
         //}
+        public ActionResult Save()
+        {
 
+            var file = Request.Files[0];
+            if (file != null)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                file.SaveAs(Server.MapPath("/images/" + fileName));
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+
+            }
+
+            return View();
+        }
 
     }
 }
